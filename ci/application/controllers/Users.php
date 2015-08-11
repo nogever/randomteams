05e28c6e9d0a0a055c69c70d585e39ad98de7f56
Sean Kim <nogever@gmail.com>
2015-08-11 18:26:53 -0400

    initial commit

---
+++ ci/application/controllers/Users.php
@@ -0,0 +1,67 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Users extends CI_Controller {
+    public function index() {
+		$this->load->database();
+		
+		$q_players = $this->db->query("SELECT * FROM users where user_type = 'player'");
+		$players = $q_players->result_array();
+		$total_players = $q_players->num_rows();
+		$total_number_of_teams = floor($total_players / 18);
+		$team_names = array('Bobs', 'Cats', 'Tigers', 'Lions', 'Blues', 'Reds', 'Yankees', 'Nuggets', 'Peanuts', 'Boys', 'Girls');
+		$teams_keys = array_rand($team_names, $total_number_of_teams);
+		
+		// sort players by ranking in reverse
+		usort($players, function ($a, $b) {
+			if($a['ranking']==$b['ranking']) return 0;
+    		return $a['ranking'] < $b['ranking'] ? 1 : -1;
+		});
+		
+		// generate team objects 
+		for ($x = 0; $x < $total_number_of_teams; $x += 1) {
+			$team = new stdClass();
+			$team->_name = $team_names[$teams_keys[$x]];
+			$team->_players = array();
+			$teams[] = $team;
+		}
+		
+		// extract goalies from players
+		$goalies = array();
+		foreach ($players as $key=>$player) {
+			if ($player['can_play_goalie'] == 1) {
+				$goalie = $player;
+				unset($players[$key]);
+				$goalies[] = $goalie;
+			}
+		}
+		
+		// assgin players to teams
+		foreach ($players as $key=>$player) {
+			$team_key = $key % $total_number_of_teams;
+			if (count($teams[$team_key]->_players) < 22) {
+				$teams[$team_key]->_players[] = 	$player;
+			}
+		}
+		
+		// assign goalies to teams
+		foreach ($goalies as $key=>$goalie) {
+			$team_key = $key % $total_number_of_teams;
+			if (count($teams[$team_key]->_players) < 22) {
+				$teams[$team_key]->_players[] = $goalie;
+			}
+		}
+		
+		// calculate the average players score per team
+		foreach ($teams as $team) {
+			$score = 0;
+			foreach ($team->_players as $p) {
+				$score = $p['ranking'] + $score;
+			}
+			$team->_players_score = $score / count($team->_players);
+		}
+		
+		$data['teams'] = $teams;
+
+       	$this->load->view('users', $data);
+    }
+}
---
+++ ci/application/views/users.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html>
+<head>
+    <title>Teams</title>
+    <?php // echo link_tag('assets/css/main.css'); ?>
+    <style>
+	body {
+		padding: 0;
+		margin: 0;
+	}
+	.team {
+		width: 25%;
+		float: left;
+		padding: 30px;
+		margin: 0;
+		box-sizing: border-box;
+		min-height: 600px;
+	}
+	.players {
+		box-sizing: border-box;
+	}
+	.goalie {
+		color: #CE5C5E;
+		margin-left: 5px;
+		font-size: 70%;	
+	}
+</style>
+</head>
+<body>
+    <div class="teams">
+        <?php
+        foreach ($teams as $team) {
+			echo '<div class="team">';
+            echo '<h3>Team ' . $team->_name . '</h3>';
+			echo '<p>Ranking: ' . number_format($team->_players_score, 1, '.', '') . '</p>';			
+			echo '<ul class="players">';
+            foreach ($team->_players as $player) {
+				echo '<li>' . $player['last_name'] . ', ' . $player['first_name'];
+				if ($player['can_play_goalie'] == 1) {
+					echo '<span class="goalie">G</span>';
+				}
+				echo '</li>';
+			}
+			echo '</ul>';
+			echo '</div>';
+        }
+        ?>
+    </div>
+    
+</body>
+</html>
